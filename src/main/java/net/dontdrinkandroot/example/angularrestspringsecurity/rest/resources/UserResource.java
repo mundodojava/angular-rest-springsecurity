4b67b0dbf1797bb9daa44e61dbe6d405ca9b879f
Ismael Gomes <ismaelcosta1000@gmail.com>
2014-05-12 14:47:49 -0300

    initial commit

--- src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/resources/UserResource.java
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/rest/resources/UserResource.java
@@ -29,6 +29,7 @@ import org.springframework.stereotype.Component;
 
 @Component
 @Path("/user")
+//@Api(value="/person",description="person api")
 public class UserResource {
 
 	@Autowired
---
+++ src/main/java/net/dontdrinkandroot/example/angularrestspringsecurity/swagger/MyCustomBootstrap.java
@@ -0,0 +1,19 @@
+package net.dontdrinkandroot.example.angularrestspringsecurity.swagger;
+
+import javax.servlet.http.HttpServlet;
+
+public class MyCustomBootstrap extends HttpServlet {
+
+/*	private static final long serialVersionUID = 100L;
+	static {
+		ConfigFactory.config().setBasePath("http://localhost:8080/angular-rest-springsecurity");
+
+		// use the following as another option
+		
+		 * BeanConfig bean = new BeanConfig(); bean.setScan(true);
+		 * bean.setResourcePackage("org.resteasy.rest");
+		 * bean.setBasePath("http://localhost:8080/spring");
+		 * bean.setVersion("1.0");
+		 
+	}*/
+}
